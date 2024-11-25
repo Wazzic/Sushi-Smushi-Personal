ccdd2198b6caf6f1622d4a94a0d285da4bdb8e6c
Mark Hanley <markthing810@gmail.com>
2024-11-25 19:00:57 +0000

    Updated Levels

--- Assets/Scripts/Other/StickyPlatform.cs
+++ Assets/Scripts/Other/StickyPlatform.cs
@@ -6,13 +6,15 @@ public class StickyPlatform : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Salmon")
+        //if (collision.gameObject.name == "Salmon")
+        if (collision.gameObject.CompareTag("Player"))
             collision.gameObject.transform.SetParent(transform);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Salmon")
+        //if (collision.gameObject.name == "Salmon")
+        if (collision.gameObject.CompareTag("Player"))
             collision.gameObject.transform.SetParent(null);
     }
 }
